bfdcc89ba00ae9dd9c337b7d502630f112a3c115
SahithiMy <98326242+SahithiMy@users.noreply.github.com>
2023-06-16 10:40:22 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+
+	filePath := "2023-6-12-15.json"
+
+	// Read the JSON file
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening JSON file:", err)
+		return
+	}
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	// Set up the request
+	var data []map[string]interface{}
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON data: %v", err)
+	}
+	client := &http.Client{Transport: tr}
+	url := "https://ec2-13-208-220-145.ap-northeast-3.compute.amazonaws.com/api/v0/events/data-collector"
+	token := "yWTSCdCOQYSHVhJOLyQv6JSwakw="
+
+	for i := range data {
+		data[i]["type"] = "inspec_report"
+		data[i]["node_uuid"] = data[i]["node_id"]
+		data[i]["report_uuid"] = data[i]["id"]
+		data[i]["fqdn"] = data[i]["node_name"]
+		if data[i]["chef_tags"] == nil {
+			data[i]["chef_tags"] = []string{}
+		}
+		if data[i]["recipes"] == nil {
+			data[i]["recipes"] = []string{}
+		}
+		if data[i]["ipaddress"] == nil {
+			data[i]["ipaddress"] = ""
+		}
+		if data[i]["source_fqdn"] == nil {
+			data[i]["source_fqdn"] = ""
+		}
+		if data[i]["chef_tags"] == nil {
+			data[i]["chef_tags"] = []string{}
+		}
+		delete(data[i], "id")
+		data[i] = getProfiles(data[i])
+		data[i] = getEndTime(data[i])
+
+		var bodyJSON io.Reader = nil
+		if data[i] != nil {
+			var err error
+			// TODO: @afiune check panic!?
+			bodyJSON, err = JSONReader(data[i])
+			if err != nil {
+				return
+			}
+		}
+		request, err := http.NewRequest("POST", url, bodyJSON)
+
+		if err != nil {
+			fmt.Println("Error creating HTTP request:", err)
+			return
+		}
+		request.Header.Add("api-token", token)
+		request.Header.Add("Content-Type", "application/json")
+		request.Header.Add("Accept", "*/*")
+
+		// Send the request
+		response, err := client.Do(request)
+		if err != nil {
+			fmt.Println("Error sending HTTP request:", err)
+			return
+		}
+
+		// Check the response
+		if response.StatusCode == 200 {
+			fmt.Println("DataCollector API call successful", response.Status, i)
+		} else {
+			fmt.Println("DataCollector API call failed", response.Status)
+		}
+	}
+
+}
+
+func JSONReader(v interface{}) (r io.Reader, err error) {
+	if debug_on() {
+		jsonout, err := json.Marshal(v)
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+		}
+		fmt.Printf("\n\nJSON IN: %+v \n JSON ERR: %+v\n", string(jsonout), err)
+	}
+	buf := new(bytes.Buffer)
+	err = json.NewEncoder(buf).Encode(v)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+	}
+	r = bytes.NewReader(buf.Bytes())
+	return
+}
+
+func debug_on() bool {
+	return false
+}
+
+func getProfiles(data map[string]interface{}) map[string]interface{} {
+	for _, profile := range data["profiles"].([]interface{}) {
+
+		if profileMap, ok := profile.(map[string]interface{}); ok {
+
+			if profileMap["supports"] == nil {
+				profileMap["supports"] = []string{}
+			}
+			if profileMap["maintainer"] == nil {
+				profileMap["maintainer"] = ""
+			}
+			if profileMap["groups"] == nil {
+				profileMap["groups"] = []string{}
+			}
+			if profileMap["license"] == nil {
+				profileMap["license"] = ""
+			}
+			for _, control := range profileMap["controls"].([]interface{}) {
+				if controls, ok := control.(map[string]interface{}); ok {
+					controls["tags"] = []string{}
+					delete(controls, "tags")
+					if controls["refs"] == nil {
+						controls["refs"] = []string{}
+						delete(controls, "refs")
+					}
+				}
+			}
+
+		}
+	}
+	return data
+}
+
+func getEndTime(data map[string]interface{}) map[string]interface{} {
+	if endtime, ok := data["end_time"].(map[string]interface{}); ok {
+
+		name := endtime["seconds"]
+
+		str := fmt.Sprintf("%v", name)
+		intVal, err := strconv.ParseFloat(str, 64)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return nil
+		}
+		delete(data, "end_time")
+		endTime := time.Unix(int64(intVal), 0)
+		formattedTime := endTime.UTC().Format("2006-01-02T15:04:05Z")
+		data["end_time"] = formattedTime
+
+	}
+	return data
+}
